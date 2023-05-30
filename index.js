10e49fd3d9608d71090b372bf051aa2c2ed547ef
AmakoveBridgit <amakovebridgit@gmail.com>
2023-05-30 17:11:09 +0300

    added changes on the Fetch API's

--- index.js
+++ index.js
@@ -1,45 +1,30 @@
 
-
-let productContainer=document.getElementById('products')
-const getProducts=()=>{
-     return fetch(`https://dummyjson.com/products`)
-      .then(response=>response.json())
-      .then(response=>response)
-      .catch(error=>error.message)
-  };
- const displayProducts=async()=>{
-       const products= await getProducts();
-       console.log(products.products)
-      products.products.map(item=>{
-          let div=document.createElement('div')
-          div.className='items'
-
-        let img=document.createElement('img')
-          let name=document.createElement('h2')
-          let describe=document.createElement('p')
-           let prices=document.createElement('p')
-          let ratings=document.createElement('p')
-          let discount=document.createElement('p')
-
-        img.src=item.imageUrl
-         name.innerHTML=item.title;
-         describe.innerHTML=item.description;
-         prices.innerHTML= `Price: $` + item.price;
-          ratings.innerHTML=`Rating: $` + item.rating;
-         discount.innerHTML=`Discount: `+ item.discountPercentage + `%`;
-         div.appendChild(img)
-           div.appendChild(name)
-           div.appendChild(describe)
-         div.appendChild(prices)
-          div.appendChild(ratings)
-           div.appendChild(discount)
-          productContainer.appendChild(div)
-       });
-   };
-   displayProducts();
   
-  
-  
+   function generateProductCards(data) {
+    const productContainer = document.getElementById("products");
+    data.forEach((product) => {
+      const card = document.createElement("div");
+      card.classList.add("product-card");
+      card.innerHTML = `
+              <img id="image" src="${product.thumbnail}" alt="${product.name}" />
+              <h2>${product.title}</h2>
+              <div class='productPrice'>
+              <p>${product.price}</p>
+              <button>ADD TO CART</button>
+              </div>
+          `;
+      productContainer.appendChild(card);
+    });
+  }
+  function fetchProducts() {
+    fetch("https://dummyjson.com/products")
+      .then((response) => response.json())
+      .then((data) => {
+        const returnedData = data.products.slice(0, 10);
+        generateProductCards(returnedData);
+    });
+  }
+  fetchProducts()
   
   
   
